c826c09f26c746ed784911af85ad55954d7425bf
KutembaChinambu <kutembachinambu@rocketmail.com>
2023-12-17 17:11:18 +0200

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -15,9 +16,12 @@ mongoose.connect(process.env.Mongo)
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3001, () => {
     console.log('server is running on port 3001');
 
     }
 );
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
